d84ce2e5fe2d1c104b6aca7185ef7601ad196051
sennacheribest <sennacheribest@gmail.com>
2021-06-10 08:52:16 +0800

    define variables and add some logic

--- doupdates.sh
+++ doupdates.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/bash
 #
-dnf check-update
+# Initialize variables
+_CHECK=1
+_DO_REBOOT=0
+
+# Main body of the program
+# first we decide whether to do the updates or just check whether any are availables
+if [ ${_CHECK} == 1 ]
+then
+	# Check for updates
+	dnf check-update
+fi
+
+# Update the man page
 mandb
+
+# Update the man pages database
 grub2-mkconfig -o /boot/grub2/grub.cfg
-# reboot
+
+# Reboot if necessary
+if [ ${_DO_REBOOT} == 1 ]
+then
+	reboot
+else
+	echo "Not rebooting."
+fi
