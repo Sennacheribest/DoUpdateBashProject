010b14b1b642fd5c9ec0e886e9b28cef2b7c5e74
sennacheribest <sennacheribest@gmail.com>
2021-06-10 15:00:08 +0800

    process doupdates command options

--- doupdates.sh
+++ doupdates.sh
@@ -1,20 +1,41 @@
 #!/usr/bin/bash
 #
 # Initialize variables
-_CHECK=1
+#=====================
+_CHECK=0
 _DO_REBOOT=0
 _NEEDS_REBOOT=0
 _UPDATES_AVAILABLE=0
+#=====================
 
 # Check for root
+#=====================================================================
 if [ ${UID} -ne 0 ]
 then
 	echo "You don't have superuser privileges to run this script!"
 	exit 1
 fi
+#=====================================================================
 
 # Main body of the program
 # first we decide whether to do the updates or just check whether any are availables
+
+# Process command input options
+#================================================
+while getops ":c:r" _OPTION;
+do
+	case ${_OPTION} in
+		# Check option enablement
+		c) _CHECK=1;;
+		# Reboot option enablement
+		r) _DO_REBOOT=1;;
+		# Incorrect options
+		\?) echo "ERROR: invalid option."
+		exit 1;;
+	esac
+done
+#================================================
+
 if [ ${_CHECK} == 1 ]
 then
 	# Check for updates
@@ -22,15 +43,21 @@ then
 fi
 
 # Update the man page
+#====
 mandb
+#====
 
 # Update the man pages database
+#=====================================
 grub2-mkconfig -o /boot/grub2/grub.cfg
+#=====================================
 
 # Reboot if necessary
+#============================
 if [ ${_DO_REBOOT} == 1 ]
 then
 	reboot
 else
 	echo "Not rebooting."
 fi
+#============================
